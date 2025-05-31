1805df2789591fef0d0baeb3bcbc6dd38b42cee6
RizwanKmc <rizwanssv@gmail.com>
2025-05-31 12:52:07 +0530

    add not found page

---
+++ src/app/not_found.tsx
@@ -0,0 +1,10 @@
+// app/not-found.tsx
+export default function NotFound() {
+    return (
+      <div className="text-center mt-20">
+        <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+        <p className="mt-2 text-gray-500">Sorry, the page you&apos;re looking for doesn&apos;t exist.</p>
+      </div>
+    )
+  }
+  
